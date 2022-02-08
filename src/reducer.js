92cb3611db001437d999924857e03c7aca2761d2
Wilfred <wilfredbayudan@gmail.com>
2022-02-08 12:27:44 -1000

    commit tree eed8ad2f8669ca33cfa99552dad6d740d7b92e95

--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,10 @@
-let state = { count: 0 };
+let state;
 
-function changeState(state, action) {
+function changeState(state = { count: 0 }, action) {
   switch (action.type) {
     case "counter/increment":
       return { count: state.count + 1 };
+      
     default:
       return state;
   }
@@ -18,3 +19,5 @@ function render() {
   const app = document.querySelector("#app");
   app.textContent = state.count;
 }
+
+dispatch({ type: "@@INIT" });
